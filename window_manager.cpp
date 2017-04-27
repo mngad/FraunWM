5687bfb4aaee5e14e7bcd5e88f08b18469066d41
fraun <gday1992@gmail.com>
2017-04-27 20:32:46 +0100

    added some error handling

--- window_manager.cpp
+++ window_manager.cpp
@@ -25,5 +25,50 @@ WindowManager::~WindowManager() {
 }
 
 
-void WindowManager::Run() {//TODO}
+void WindowManager::Run() {
+	wm_detected_ = false;
+	XSetErrorHandler(&WindowManager::OnWMDetected);
+	XSelectInput(
+			display_,
+			root_,
+			SubstructureRedirectMask | SubstructureNotifyMask);
+	
+	XSync(display_, false);
+
+	if (wm_detected_) {
+	
+		LOG(ERROR) << "Detected another WM on disp"
+			<<XDisplayString(display_);
+		return;
+
+	}
+	XSetErrorHandler(&WindowManager::OnXError);
+
+	//main loop
+
+
+}
+
+int WindowManager::OnWMDetected(Display* display, XErrorEvent* e) {
+
+	CHECK_EQ(static_cast<int>(e->error_code), BadAccess);
+	wm_detected_ = true;
+	return 0;
+
+}
+
+int WindowManager::OnXError(Display* display, XErrorEvent* e) {
+	const int MAX_ERROR_TEXT_LENGTH = 1024;
+	char error_text[MAX_ERROR_TEXT_LENGTH];
+	XGetErrorText(display, e->error_code, error_text, sizeof(error_text));
+	LOG(ERROR) << "Received X error:\n"
+	           << "    Request: " << int(e->request_code)
+	           << " - " << XRequestCodeToString(e->request_code) << "\n"
+	           << "    Error code: " << int(e->error_code)
+	           << " - " << error_text << "\n"
+	           << "    Resource ID: " << e->resourceid;
+	 // The return value is ignored.
+	return 0;
+
+}
 
--- window_manager.hpp
+++ window_manager.hpp
@@ -1,12 +1,14 @@
 extern "C" {
-	#inlcude <X11/Xlib.h>
+	#include <X11/Xlib.h>
 }
 #include <memory>
 
 class WindowManager {
 	public:
 		static ::std::unique_ptr<WindowManager> Create();
+		
 		~WindowManager();
+		
 		void Run();
 
 	private:
@@ -15,5 +17,10 @@ class WindowManager {
 		Display* display_;
 
 		const Window root_;
+		
+		static int OnXError(Display* display, XErrorEvent* e);
+		
+		static int OnWMDetected(Display* display, XErrorEvent* e);
 
+		static bool wm_detected_;
 };
